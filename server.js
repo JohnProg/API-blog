ee65ae92756e5504eadc6ce6bf74bb75c809611e
john <jmachahuay@belatrixsf.com>
2016-05-13 12:53:32 -0500

    Base of the app

--- server.js
+++ server.js
@@ -1,159 +1,206 @@
 #!/bin/env node
 //  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
+var config      = require('./config'); // get our config file
+//var mongodb     = require('mongolab-provider').init('liveupload', config.api_settings);
+var express     = require('express');
+var mongoose    = require('mongoose');
+var bodyParser  = require('body-parser');
+var app         = express();
+
+var server = require('http').createServer(app);
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 5000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var io = require('socket.io').listen(server);
 
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+// Mongoose Schema definition
+Schema = new mongoose.Schema({
+  id       : String, 
+  title    : String,
+  completed: Boolean
+}),
 
-    //  Scope.
-    var self = this;
+Todo = mongoose.model('Todo', Schema);
 
+var MONGO_URL = '127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME;
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+// if OPENSHIFT env variables are present, use the available connection info:
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+    MONGO_URL = process.env.OPENSHIFT_MONGODB_DB_URL +
+    process.env.OPENSHIFT_APP_NAME;
+}
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+// Connect to mongodb
+var connect = function () {
+    mongoose.connect(MONGO_URL);
+};
+connect();
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+var db = mongoose.connection;
 
+db.on('error', function(error){
+    console.log("Error loading the db - "+ error);
+});
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+db.on('disconnected', connect);
 
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
 
 
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
+//app.set('superSecret', config.secret); 
+
+// parse application/json
+app.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api', function (req, res) {
+    res.json(200, {msg: 'OK' });
+})
+
+app.get('/api/todos', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#query_Query-find
+    Todo.find( function ( err, todos ){
+      res.json(200, todos);
+    });
+  })
+
+app.post('/api/todos', function (req, res) {
+    var todo = new Todo( req.body );
+    todo.id = todo._id;
+    // http://mongoosejs.com/docs/api.html#model_Model-save
+    todo.save(function (err) {
+      res.json(200, todo);
+    });
+  })
+
+app.del('/api/todos', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#query_Query-remove
+    Todo.remove({ completed: true }, function ( err ) {
+      res.json(200, {msg: 'OK'});
+    });
+  })
+
+app.get('/api/todos/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    Todo.findById( req.params.id, function ( err, todo ) {
+      res.json(200, todo);
+    });
+  })
+
+app.put('/api/todos/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    Todo.findById( req.params.id, function ( err, todo ) {
+      todo.title = req.body.title;
+      todo.completed = req.body.completed;
+      // http://mongoosejs.com/docs/api.html#model_Model-save
+      todo.save( function ( err, todo ){
+        res.json(200, todo);
+      });
+    });
+  })
+
+app.del('/api/todos/:id', function (req, res) {
+    // http://mongoosejs.com/docs/api.html#model_Model.findById
+    Todo.findById( req.params.id, function ( err, todo ) {
+      // http://mongoosejs.com/docs/api.html#model_Model.remove
+      todo.remove( function ( err, todo ){
+        res.json(200, {msg: 'OK'});
+      });
+    });
+  })
+
+/*
+app.use(bodyParser.raw());
+
+app.use(function(req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, X-Auth-Token, x-key");
+
+    res.setHeader('Content-Type', 'application/json');
+    next();
+
+});
 
 
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
 
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
+io.sockets.on("connection", function(socket) {
+    socket.on('something', function(data){
+        console.log(data);
+        io.sockets.emit('saludo', "hola");
+    });
+});
+
+// Authentication
+var jwt = require('jsonwebtoken');
+var expiresInSession = 86400; // expires in 24 hours
+
+var errorResponse = {
+    status: "ERROR"
+};
+var authResponse = {
+    status: "OK"
+};
+
+function authorized(req, res, next) {
+    var header = req.headers;
+    console.log('header.authorization:' + header['x-auth-token']);
+     // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || header['x-auth-token'];
+    // decode token
+    if (token) {
+        jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+            if(err) {
+                res.status(403);
+                res.send(errorResponse);
+            } else {
+                req.decoded = decoded;
+                next();
+            }
         });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
+    } else {
+        return res.status(403).send({ 
+            success: false, 
+            message: 'No token provided.' 
         });
-    };
+        
+    }
+}
 
-};   /*  Sample Application.  */
+
+app.get('/', function(req, res) {
+    res.send("Hello John");
+});
+
+app.get('/notifications', authorized, function(req, res) {
+    // res.send(Notifications);
+    mongodb.documents('alerts', {sort: {"created_at":-1}}, function (err, data) {
+        var result = null;
+        if (err || data.length === 0) {
+            result = errorResponse;
+        } else {
+            result = data;
+        }
+        res.json(result);
+    });
+});
 
 
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+// Verifying token
+app.get('/check', authorized, function(req, res) {
+    res.json(req.decoded);
+});
+app.get('/users/', authorized, function(req, res) {
+    res.json({name: "name"});
+});
+*/
+app.use(express.static(__dirname + '/'))
+//server.listen(server_port, server_ip_address);
+server.listen(server_port, function () {
+  console.log('Server listening at port %d', server_port);
+});
